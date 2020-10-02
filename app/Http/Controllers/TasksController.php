1855a7f2e1d94306abc2cfbcff3efeb4574fc70e
kouichi.shimizu <programming.gangi@gmail.com>
2020-10-02 06:25:34 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,7 +44,13 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required|max255',
+        ]);
+        
         $task = new Task;
+        $task->title = $request->title;
         $task->content = $request->content;
         $task->save();
 
@@ -90,7 +96,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $task->validate([
+            'title' => 'required|max:255',
+            'content' => 'required|max:255'
+        ]);
+        
         $task = Task::findOrFail($id);
+        $task->title = $request->title;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,6 +9,7 @@
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>タイトル</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -16,6 +17,7 @@
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ %task->title }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
@@ -23,6 +25,6 @@
         </table>
     @endif
     
-    {!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
